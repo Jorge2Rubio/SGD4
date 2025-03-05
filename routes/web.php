3eac256fa7d8b016dfd89c95882d0570d22f084e
Jorge2Rubio <jorgepatrickrubio@gmail.com>
2025-03-05 10:37:12 +0800

    first

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pay', [PaymentController::class, 'pay']);
+Route::get('/success', [PaymentController::class, 'success']);
+Route::get('/listUser', [PaymentController::class, 'listUser']);
