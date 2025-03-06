75b309e2f37bdf8aa19a3307f9fd6a53b531728f
Jorge2Rubio <jorgepatrickrubio@gmail.com>
2025-03-06 16:35:37 +0800

    added about route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function(){
+    return view('about');
+});
+
+
+
 Route::get('/pay', [PaymentController::class, 'pay']);
 Route::get('/success', [PaymentController::class, 'success']);
 Route::get('/listUser', [PaymentController::class, 'listUser']);
